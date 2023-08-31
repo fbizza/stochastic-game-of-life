0e7f6dc609909104ae7d19d7bcab2e41af53f243
fbizza <francesco.bizzarri@stud.unifi.it>
2023-08-31 18:24:08 +0200

    Add code for replication of experiment 7

---
+++ experiments/fig_7.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from scipy import stats
+from tqdm import tqdm
+
+
+def update_grid(grid, p_d, p_l):
+    new_grid = np.copy(grid)
+    rows, cols = grid.shape
+    living_cells = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            # Periodic boundary
+            live_neighbors = (
+                grid[(i - 1) % rows, (j - 1) % cols] + grid[(i - 1) % rows, j] + grid[(i - 1) % rows, (j + 1) % cols] +
+                grid[i, (j - 1) % cols] + grid[i, (j + 1) % cols] +
+                grid[(i + 1) % rows, (j - 1) % cols] + grid[(i + 1) % rows, j] + grid[(i + 1) % rows, (j + 1) % cols]
+            )
+
+            # Update living cells
+            if grid[i, j] == 1:
+                living_cells += 1
+                if live_neighbors < 2 or live_neighbors > 3:
+                    new_grid[i, j] = 0
+                elif live_neighbors == 2 and np.random.random() >= p_l:
+                    new_grid[i, j] = 0
+
+            # Update dead cells
+            else:
+                if live_neighbors == 3:
+                    new_grid[i, j] = 1
+                elif live_neighbors == 2 and np.random.random() <= p_d:
+                    new_grid[i, j] = 1
+
+    density = living_cells/(grid_size[0]*grid_size[1])
+    return new_grid, density
+
+
+def run_simulation(grid, p_d, p_l, i, n):
+    densities = []
+    for _ in tqdm(range(n_time_steps), desc=f"Running {i+1} of {n} simulations {grid_size}", position=0):
+        new_grid, density = update_grid(grid, p_d, p_l)
+        grid = new_grid
+        densities.append(density)
+    return densities
+
+grid_size = (50, 50)
+initial_probability = 0.1
+n_time_steps = 10000
+n_samples = 16
+pd = 0
+pl_list = list(np.linspace(0.99, 0.995, math.floor(n_samples/4))) + \
+          list(np.linspace(0.996, 0.9985, math.floor(n_samples/4))) +\
+          list(np.linspace(0.9986, 0.9994, math.floor(n_samples/4))) +\
+          list(np.linspace(0.9995, 1.0, math.ceil(n_samples/4)))
+
+
+phi_list = []
+
+for i in range(n_samples):
+    grid = np.random.choice([0, 1], grid_size, p=[1 - initial_probability, initial_probability])
+    densities = run_simulation(grid, pd, pl_list[i], i, n_samples)
+    last_1000 = densities[-1000:]
+    rounded_last_1000 = np.round(last_1000, 3)
+    mode_result = stats.mode(rounded_last_1000)
+    mode_value = mode_result.mode[0]
+    print(f"\nMode density (pl = {round(pl_list[i], 4)}): {mode_value}")
+    phi_list.append(mode_value)
+
+
+plt.plot(pl_list, phi_list)
+plt.title('Life density \u03A6 along edge (p$_d$ = 0, p$_l$)')
+plt.xlabel('p$_l$')
+plt.ylabel('\u03A6')
+plt.ylim(0, 0.1)
+# x_ticks = [10, 100, 1000]
+# plt.xticks(x_ticks)
+# plt.yticks(y_ticks)
+plt.show()
